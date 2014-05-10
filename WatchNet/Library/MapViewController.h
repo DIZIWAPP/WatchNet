0ded29d1583fa65f4f1a52acd0958beafee67be6
Jasmeet Singh <jasmeetsingh@jasmeetsingh-macbookpro2.roam.corp.google.com>
2014-05-10 13:41:20 -0700

    Adds a Map View Controller

---
+++ WatchNet/Library/MapViewController.h
@@ -0,0 +1,24 @@
+//
+//  ParseStarterProjectViewController.h
+//  WatchNet
+//
+//  Created by Jasmeet Singh on 5/10/14.
+//  Copyright (c) 2014 RescueMe. All rights reserved.
+//
+
+#import <CoreLocation/CoreLocation.h>
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+
+@interface MapViewController : UIViewController <CLLocationManagerDelegate>{
+	
+	CLLocationManager *locationManager;
+	
+    MKMapView *mapView;
+}
+
+
+@property (nonatomic, retain) IBOutlet MKMapView *mapView;
+
+@end
